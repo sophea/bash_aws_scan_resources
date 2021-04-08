eec3255b5f6aac40da531f58b09b0154abb3f019
Sophea Mak <mak.sophea@jtrustsystem.co.jp>
2021-04-08 14:15:25 +0700

    add exclude instances

--- stop_ec2.py
+++ stop_ec2.py
@@ -6,21 +6,19 @@ import os
 
 today=datetime.today().strftime('%Y-%m-%d')
 
-ec2 = boto3.client('ec2')
-
 region = os.getenv('REGION')
-
-isHoliday = False
+ec2 = boto3.resource('ec2',region_name=region)
 
 def lambda_handler(event, context):
     
+    isHoliday = False
 
     print("today :" + today)
     print("event " + str(event))
-    print("context " + str(context))
+   # print("context " + str(context))
     
     days = event["days"]
-
+    excludes = event["excludes"]
     if today in days :
         print ("found - it is public holiday : " + today)
         isHoliday = True
@@ -29,13 +27,14 @@ def lambda_handler(event, context):
         print ("It is not public holiday " + today)
         return {'body' : "nothing to process"}
     ## stop all running instances    
-    ec2 = boto3.resource('ec2',region_name=region)
+   
     instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
     RunningInstances = [instance.id for instance in instances]
     for i in RunningInstances:
-        #stoppingInstances = ec2.instances.stop(i)
-        #print(stoppingInstances)
-        print ("stop instances : " + i)
+        if (not i in excludes) :
+            #stoppingInstances = ec2.instances.stop(i)
+            print ("stop instances : " + i)
+       
     
     return {
         'statusCode': 200,
@@ -50,5 +49,6 @@ def lambda_handler(event, context):
 #     [     "2021-04-14",
 #     "2021-04-15", 
 #     "2021-04-16", 
-#      ] 
+#      ],
+#     "excludes" : [ "i-0d62303a89e7839fe","i-0af520a0246b8a3e2"]
 # }
